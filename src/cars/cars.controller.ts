3f5ab214abe1dc4add3380b29334001bdb0e0d80
Jesus Francisco Aguilera Guajardo <aguilera.jfrancisco6@gmail.com>
2024-12-27 00:44:51 -0600

    basic CRUD

--- src/cars/cars.controller.ts
+++ src/cars/cars.controller.ts
@@ -1,5 +1,8 @@
-import { Body, Controller, Get, Param, ParseIntPipe, Patch, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, ParseUUIDPipe, Patch, Post } from '@nestjs/common';
+import { CreateCarDto } from './DTO/create-car.dto';
+import { UpdateCarDto } from './DTO/update-car.dto';
 import { CarsService } from './cars.service';
+import { ICar } from './interfaces/car.interface';
 
 @Controller('cars')
 export class CarsController {
@@ -14,17 +17,21 @@ export class CarsController {
         return this.carsService.findAll();
     }
     @Get(":id")
-    getCarById(@Param('id', ParseIntPipe) id: number){
+    getCarById(@Param('id', ParseUUIDPipe) id: string){
         console.log({id});
-        return this.carsService.findById(Number(id));
+        return this.carsService.findById(id);
         
     }
     @Post()
-    createCar(@Body() body: any){
-        return body;
+    createCar(@Body() createCarDto: CreateCarDto): ICar{
+        return this.carsService.create(createCarDto);
     }
-    @Patch()
-    updateCar(){
-        
+    @Patch(":id")
+    updateCar(@Param('id', ParseUUIDPipe)id: string, @Body() updateCarDto: UpdateCarDto): ICar{
+        return this.carsService.update(id, updateCarDto); 
+    }
+    @Delete(":id")
+    deleteCar(@Param('id', ParseUUIDPipe) id: string): ICar{
+        return this.carsService.delete(id);
     }
 }
--- src/cars/cars.service.ts
+++ src/cars/cars.service.ts
@@ -1,20 +1,22 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
-
+import { v4 as uuid } from 'uuid';
+import { CreateCarDto, UpdateCarDto } from './dto';
+import { ICar } from './interfaces/car.interface';
 @Injectable()
 export class CarsService {
-    private cars = [
+    private cars: ICar[] = [
         {
-            id: 1,
+            id: uuid(),
             brand: 'Toyota',
             model: 'Corolla'
         },
         {
-            id: 2,
+            id: uuid(),
             brand: 'Honda',
             model: 'Civic'
         },
         {
-            id: 3,
+            id: uuid(),
             brand: 'Jeep',
             model: 'Cherokee'
         }
@@ -22,10 +24,41 @@ export class CarsService {
     findAll(){
         return this.cars;
     }
-    findById(id: number){
+    findById(id: string){
         const car = this.cars.find( car => car.id === id );
         if ( !car ) throw new NotFoundException(`Car with id '${ id }' not found`);
         
         return car;
+    }
+    create(createCarDto: CreateCarDto): ICar{
+        const newCar: ICar = {
+            id: uuid(),
+            ...createCarDto
+        }
+        this.cars.push(newCar);
+        return newCar;
+    }
+    update(id: string, updateCarDto: UpdateCarDto): ICar{
+        let carToUpdate = this.findById(id);
+        if(!carToUpdate){
+            throw new NotFoundException(`Car with id '${ id }' not found to patch`);
+        }
+        this.cars = this.cars.map(car => {
+            if(car.id === id){
+                carToUpdate = {
+                    ...carToUpdate,
+                    ...updateCarDto
+                }
+                return carToUpdate;
+            }
+            return car;
+        })
+        return carToUpdate;
+    }
+    delete(id: string): ICar{
+        const carToDelete = this.findById(id);
+        const index = this.cars.indexOf(carToDelete);
+        this.cars.splice(index, 1);
+        return carToDelete;
     }
 }
